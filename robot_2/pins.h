3e0d1bdb6ca0c4dba9991ef66174e94e2efbe98d
Martoto <dansal@alunos.utfpr.edu.br>
2022-11-06 17:08:40 -0300

    Commit inicial

---
+++ robot_2/pins.h
@@ -0,0 +1,15 @@
+#define HIGH 1
+#define LOW 0
+
+#define servo 3
+
+#define m1p1 5
+#define m1p2 6
+#define m2p1 10
+#define m2p2 9
+/*
+in1 in2 esquerd
+in3 in4 direit
+
+in 3 pino 6
+in 4 pino 5*/
