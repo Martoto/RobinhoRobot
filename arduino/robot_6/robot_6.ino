272b194a99d2971a139d2c3fbb115adeaaed167b
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2022-11-15 13:37:34 -0300

    robot6rotate

--- arduino/robot_6/robot_6.ino
+++ arduino/robot_6/robot_6.ino
@@ -4,21 +4,28 @@
 #include "Adafruit_TCS34725.h"
 #include <Adafruit_NeoPixel.h>
 
+#define BASE_PWM 60
+#define TICKS_FORWARD 42
+#define TICKS_ROTATE_90 13
+#define LOCK_TIME 500
+#define PWM_MAX_ADJ 30
+#define PWM_ADJ_KP 10
+#define BASETIME 200
+#define PWM_BASE_STUCK 8
+#define STARTUP_TICKS 2
+#define PWM_STARTUP_BOOST 150
+#define STARTUP_BOOST_TIME 150
+#define ENCODER_PPR 20
+
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
 Adafruit_NeoPixel pixels(8, 13, NEO_GRB + NEO_KHZ800);
 ServoTimer2 myservo;
-
-int motor1_velocity = 0;
-int motor2_velocity = 0;
 
 volatile byte m1e_count = 0;
 volatile byte m2e_count = 0;
 volatile unsigned long m1e_last = 0;
 volatile unsigned long m2e_last = 0;
 
-
-//Altere o numero abaixo de acordo com o seu disco encoder
-const unsigned int pulsos_por_volta = 20;
 
 void m1e_i() {
   m1e_count++;
@@ -38,8 +45,8 @@ void setLock() {
 }
 
 void setVelocity(int motor1, int motor2) {
-  motor1 = constrain(motor1, 0, 255);
-  motor2 = constrain(motor2, 0, 255);
+  motor1 = constrain(motor1, -255, 255);
+  motor2 = constrain(motor2, -255, 255);
   if (motor1 == 0) {
     analogWrite(m1p1, 0);
     analogWrite(m1p2, 0);
@@ -73,20 +80,12 @@ void resetGpio() {
 char cmdTreatment(char cmd) {
   char resp;
   if (cmd == 'w') {
-    motor1_velocity = 200;
-    motor2_velocity = 200;
     resp = cmd;
   } else if (cmd == 's') {
-    motor1_velocity = -200;
-    motor2_velocity = -200;
     resp = cmd;
   } else if (cmd == 'd') {
-    motor1_velocity = -200;
-    motor2_velocity = 200;
     resp = cmd;
   } else if (cmd == 'a') {
-    motor1_velocity = 200;
-    motor2_velocity = -200;
     resp = cmd;
   } else if (cmd == 'o') {
     myservo.write(2250);
@@ -152,15 +151,20 @@ void setup() {
   resetGpio();
 }
 
+void mstate_start_advance(int distance) {
+
+}
+
 void loop() {
   int base = 0;
   int dj = 0;
   unsigned int m1e_total_count = 0;
   unsigned int m2e_total_count = 0;
   unsigned long timeold = 0;
-  int b = 70;
-
-  int mstate = 0;
+  
+  // 0 is forward
+  // 10 is rotate 90
+  int mstate = 10;
   unsigned long mstate_timer = 0;
 
   while (1) {
@@ -173,7 +177,6 @@ void loop() {
 
     switch (mstate) {
       case 0:
-
         if (millis() - mstate_timer > 2000) {
           mstate = 1;
           mstate_timer = millis();
@@ -184,34 +187,31 @@ void loop() {
 
           base = 0;
           dj = 0;
-          pixels.setPixelColor(0, pixels.Color(200, 0, 0));
           Serial.println("0-1");
-          setVelocity(115, 115);
+          setVelocity(100, 100);
         }
         break;
 
-
       case 1:
-        if ((m1e_count >= 2) && (m2e_count >= 2)) {
+        if ((m1e_count >= STARTUP_TICKS) && (m2e_count >= STARTUP_TICKS)) {
           timeold = millis();
           m1e_count = 0;
           m2e_count = 0;
-          setVelocity(b, b);
+          setVelocity(BASE_PWM, BASE_PWM);
 
           Serial.println("1-2");
           mstate = 2;
           mstate_timer = millis();
-        } else if (millis() - mstate_timer > 100) {
-          int m1 = (m1e_count >= 2) ? 80 : 150;
-          int m2 = (m2e_count >= 2) ? 80 : 150;
+        } else if (millis() - mstate_timer > STARTUP_BOOST_TIME) {
+          int m1 = (m1e_count >= STARTUP_TICKS) ? BASE_PWM : PWM_STARTUP_BOOST;
+          int m2 = (m2e_count >= STARTUP_TICKS) ? BASE_PWM : PWM_STARTUP_BOOST;
           setVelocity(m1, m2);
         }
         break;
 
-
       case 2:
-        //if ((m1e_total_count + m1e_count + m2e_total_count + m2e_count) >= 28) {
-        if (max(m1e_total_count + m1e_count, m2e_total_count + m2e_count) >= 15) {
+        if ((m1e_total_count + m1e_count + m2e_total_count + m2e_count) >= TICKS_FORWARD) {
+        //if (max(m1e_total_count + m1e_count, m2e_total_count + m2e_count) >= 15) {
           base = 0;
           dj = 0;
           m1e_total_count = 0;
@@ -223,15 +223,14 @@ void loop() {
           mstate = 3;
           mstate_timer = millis();
           setLock();
+          //setVelocity(0, 0);
           Serial.println("2-3");
-          pixels.setPixelColor(0, pixels.Color(0, 200, 0));
 
         } else {
           disable_encoder_interrupts();
 
-          const int BASETIME = 150;
           if ((millis() - m1e_last > BASETIME) || (millis() - m2e_last > BASETIME)) {
-            base += 8*max((millis() - m1e_last)/BASETIME, (millis() - m2e_last)/BASETIME);
+            base += PWM_BASE_STUCK*max((millis() - m1e_last)/BASETIME, (millis() - m2e_last)/BASETIME);
           }
 
           base = constrain(base, 0, 255);
@@ -239,47 +238,121 @@ void loop() {
           m1e_total_count += m1e_count;
           m2e_total_count += m2e_count;
 
-          dj = (m2e_total_count - m1e_total_count) * 25;
-          //dj = constrain(dj, -40, 40);
+          dj = (m2e_total_count - m1e_total_count) * PWM_ADJ_KP;
+          dj = constrain(dj, -PWM_MAX_ADJ, PWM_MAX_ADJ);
 
           if (dj > 0) {
-            setVelocity(base + b + dj, base + b);
+            setVelocity(base + BASE_PWM + dj, base + BASE_PWM);
           } else {
-            setVelocity(base + b, base + b - dj);
+            setVelocity(base + BASE_PWM, base + BASE_PWM - dj);
           }
-
-          Serial.print("PWM = ");
-          Serial.println(base, DEC);
-          Serial.print("dj = ");
-          Serial.println(dj, DEC);
-          Serial.print("C1 = ");
-          Serial.println(m1e_count, DEC);
-          Serial.print("C2 = ");
-          Serial.println(m2e_count, DEC);
-          Serial.print("T1 = ");
-          Serial.println(m1e_total_count, DEC);
-          Serial.print("T2 = ");
-          Serial.println(m2e_total_count, DEC);
 
           m1e_count = 0;
           m2e_count = 0;
 
           enable_encoder_interrupts();
-
-          Serial.println(millis(), DEC);
-
-          //basevel = (basevel + 3) % 255;
         }
         break;
+
       case 3:
-        if (millis() - mstate_timer > 1000) {
-          mstate = 0;
+        if (millis() - mstate_timer > LOCK_TIME) {
+          mstate = 10;
           mstate_timer = millis();
-          pixels.setPixelColor(0, pixels.Color(0, 0, 200));
           setVelocity(0, 0);
           Serial.println("3-0");
         }
         break;
+
+
+
+
+      case 10:
+        if (millis() - mstate_timer > 2000) {
+          mstate = 11;
+          mstate_timer = millis();
+          m1e_total_count = 0;
+          m2e_total_count = 0;
+          m1e_count = 0;
+          m2e_count = 0;
+
+          base = 0;
+          dj = 0;
+          Serial.println("0-1");
+          setVelocity(100, -100);
+        }
+        break;
+
+      case 11:
+        if ((m1e_count >= STARTUP_TICKS) && (m2e_count >= STARTUP_TICKS)) {
+          timeold = millis();
+          m1e_count = 0;
+          m2e_count = 0;
+          setVelocity(BASE_PWM, BASE_PWM);
+
+          Serial.println("1-2");
+          mstate = 12;
+          mstate_timer = millis();
+        } else if (millis() - mstate_timer > STARTUP_BOOST_TIME) {
+          int m1 = (m1e_count >= STARTUP_TICKS) ? BASE_PWM : PWM_STARTUP_BOOST;
+          int m2 = (m2e_count >= STARTUP_TICKS) ? -BASE_PWM : -PWM_STARTUP_BOOST;
+          setVelocity(m1, m2);
+        }
+        break;
+
+      case 12:
+        //if ((m1e_total_count + m1e_count + m2e_total_count + m2e_count) >= TICKS_ROTATE_90) {
+        if (max(m1e_total_count + m1e_count, m2e_total_count + m2e_count) >= 7) {
+          base = 0;
+          dj = 0;
+          m1e_total_count = 0;
+          m2e_total_count = 0;
+          m1e_count = 0;
+          m2e_count = 0;
+          timeold = 0;
+
+          mstate = 13;
+          mstate_timer = millis();
+          setLock();
+          //setVelocity(0, 0);
+          Serial.println("2-3");
+
+        } else {
+          disable_encoder_interrupts();
+
+          if ((millis() - m1e_last > BASETIME) || (millis() - m2e_last > BASETIME)) {
+            base += PWM_BASE_STUCK*max((millis() - m1e_last)/BASETIME, (millis() - m2e_last)/BASETIME);
+          }
+
+          base = constrain(base, 0, 255);
+
+          m1e_total_count += m1e_count;
+          m2e_total_count += m2e_count;
+
+          dj = (m2e_total_count - m1e_total_count) * PWM_ADJ_KP;
+          dj = constrain(dj, -PWM_MAX_ADJ, PWM_MAX_ADJ);
+
+          if (dj > 0) {
+            setVelocity(base + BASE_PWM + dj, -(base + BASE_PWM));
+          } else {
+            setVelocity(base + BASE_PWM, -(base + BASE_PWM - dj));
+          }
+
+          m1e_count = 0;
+          m2e_count = 0;
+
+          enable_encoder_interrupts();
+        }
+        break;
+
+      case 13:
+        if (millis() - mstate_timer > LOCK_TIME) {
+          mstate = 10;
+          mstate_timer = millis();
+          setVelocity(0, 0);
+          Serial.println("3-0");
+        }
+        break;
+
     }
   }
 }
