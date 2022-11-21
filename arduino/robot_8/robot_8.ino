b701b9f37237b3423581a49dddca87045795ee58
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2022-11-21 12:54:01 -0300

    robot8 pose wip

--- arduino/robot_8/robot_8.ino
+++ arduino/robot_8/robot_8.ino
@@ -7,7 +7,7 @@
 #define BASE_PWM 60
 #define TICKS_FORWARD 42
 #define TICKS_ROTATE_90 8
-#define LOCK_TIME 500
+#define BRAKE_TIME 500
 #define PWM_SURGE_MAX_ADJ 100
 #define PWM_SURGE_ADJ_KP 35
 #define PWM_SWAY_MAX_ADJ 30
@@ -19,7 +19,143 @@
 #define PWM_STARTUP 90
 #define STARTUP_BOOST_TIME 150
 #define ENCODER_PPR 20
+#define ANGLE_MOVE_TOLERANCE 10
 
+enum angle_e : int16_t {
+  EAST = 0,
+  SOUTH = 64,
+  WEST = 128,
+  NORTH = 191,
+  INVALID = 1000
+};
+
+angle_e angle_e_rotate_left(angle_e in) {
+  switch(in) {
+    case EAST:
+    return NORTH;
+    case SOUTH:
+    return EAST;
+    case WEST:
+    return SOUTH;
+    case NORTH:
+    return WEST;
+    default:
+    return INVALID;
+  }
+}
+
+angle_e angle_e_rotate_right(angle_e in) {
+  switch(in) {
+    case EAST:
+    return SOUTH;
+    case SOUTH:
+    return WEST;
+    case WEST:
+    return NORTH;
+    case NORTH:
+    return EAST;
+    default:
+    return INVALID;
+  }
+}
+
+/*
+const byte grid_colors[8][8] = {
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0}
+};
+
+const byte return_to_base_dir[8][8] = {
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0}
+};
+
+const byte blocked_moves[8][8] = {
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0}
+};
+*/
+
+struct Angle {
+  int16_t val = 0;
+
+  Angle(int16_t val) {
+    this->val = val;
+  }
+
+  int16_t diff(Angle b) {
+    int16_t k = (this->val - b.val) % 360;
+    return min(360 - k, k);
+  }
+
+  angle_e nearest_direction() {
+    angle_e ret;
+    int16_t mdiff = 1000;
+
+    int16_t calc;
+
+    calc = abs(this->diff(Angle{ val: EAST }));
+    if (calc < mdiff) {
+      mdiff = calc;
+      ret = EAST;
+    }
+
+    calc = abs(this->diff(Angle{ val: SOUTH }));
+    if (calc < mdiff) {
+      mdiff = calc;
+      ret = SOUTH;
+    }
+
+    calc = abs(this->diff(Angle{ val: WEST }));
+    if (calc < mdiff) {
+      mdiff = calc;
+      ret = WEST;
+    }
+
+    calc = abs(this->diff(Angle{ val: NORTH }));
+    if (calc < mdiff) {
+      mdiff = calc;
+      ret = NORTH;
+    }
+
+    return ret;
+  }
+};
+
+struct GridPosition;
+
+struct RealPosition {
+  int16_t x = 0;
+  int16_t y = 0;
+
+  RealPosition(int16_t x, int16_t y) {
+    this->x = x;
+    this->y = y;
+  }
+
+  GridPosition to_grid();
+};
+
+struct GridPosition {
+  int16_t x = 0;
+  int16_t y = 0;
+
+  GridPosition(int16_t x, int16_t y) {
+    this->x = x;
+    this->y = y;
+  }
+
+  RealPosition to_real() {
+    return RealPosition{ x: this->x * 8, y: this->y * 8 };
+  }
+};
+
+GridPosition RealPosition::to_grid() {
+  return GridPosition{ x: this->x / 8, y: this->y / 8 };
+}
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
 Adafruit_NeoPixel pixels(8, 13, NEO_GRB + NEO_KHZ800);
@@ -34,12 +170,13 @@ int16_t unstuck_boost = 0;
 uint16_t m12e_target = 0;
 uint16_t m1e_total_count = 0;
 uint16_t m2e_total_count = 0;
+Angle movement_final_angle = 0;
 
 enum class mstate_e {
   STOPPED,
   STARTUP,
   MOVING,
-  LOCKED,
+  BRAKES,
   WAITING_POSE
 };
 
@@ -55,7 +192,8 @@ enum class movement_type_e {
 
 movement_type_e movement_type = movement_type_e::FORWARD;
 // x y ang
-uint8_t pose[3] = { 0 };
+RealPosition pose_real = { 0, 0 };
+Angle pose_ang = { 0 };
 uint32_t pose_time = 0;
 
 void m1e_i() {
@@ -108,7 +246,6 @@ void resetGpio() {
   myservo.write(2250);
 }
 
-
 void openServo() {
   myservo.write(2250);
 }
@@ -142,6 +279,8 @@ void mstate_reset() {
   setVelocity(0, 0);
 }
 
+// ??????
+void mstate_start(movement_type_e movetype, int16_t target);
 void mstate_start(movement_type_e movetype, int16_t target) {
   mstate = mstate_e::STARTUP;
   mstate_timer = millis();
@@ -152,33 +291,48 @@ void mstate_start(movement_type_e movetype, int16_t target) {
   m2e_count = 0;
 
   unstuck_boost = 0;
-  
+
   pixels.setPixelColor(5, 150, 0, 0);
 
   movement_type = movetype;
   mt_setVelocity(PWM_STARTUP, PWM_STARTUP);
 }
 
+void mstate_rotation_to(Angle target);
+void mstate_rotation_to(Angle target) {
+    movement_final_angle = target;
+    int16_t diff = pose_ang.diff(target);
+    int16_t encoder_pulses = (TICKS_ROTATE_90 * abs(diff))/64;
+    mstate_start((diff < 0) ? movement_type_e::RIGHT : movement_type_e::LEFT, encoder_pulses);
+}
+
 void cmdTreatment(int cmd) {
   if (cmd == 0x1C) {
-     mstate_start(movement_type_e::FORWARD, TICKS_FORWARD);
+    mstate_start(movement_type_e::FORWARD, TICKS_FORWARD);
   } else if (cmd == 0x1F) {
-    mstate_start(movement_type_e::LEFT, TICKS_ROTATE_90);
+    pixels.setPixelColor(4, 0, 0, 150);
+    angle_e target_direction = angle_e_rotate_left(pose_ang.nearest_direction());
+    mstate_rotation_to(Angle{target_direction});
   } else if (cmd == 0x1E) {
-    mstate_start(movement_type_e::RIGHT, TICKS_ROTATE_90);
+    pixels.setPixelColor(4, 0, 0, 150);
+    angle_e target_direction = angle_e_rotate_right(pose_ang.nearest_direction());
+    mstate_rotation_to(Angle{target_direction});
   } else if (cmd == 0x1D) {
     mstate_start(movement_type_e::BACKWARDS, TICKS_FORWARD);
   } else if (cmd == 0b00000000) {
-      Serial.write('1');        
-      for (int j = 0; j < 3; j++) {
-        while (!Serial.available()) {}
-        pose[j] = Serial.read();
-        Serial.write('1');
-      }
-      pose_time = millis();
-      pixels.setPixelColor(6, pose[0], pose[1], pose[2]);
-      pixels.show();
-    } else {
+    Serial.write('1');
+    int16_t posein[3];
+    for (int j = 0; j < 3; j++) {
+      while (!Serial.available()) {}
+      posein[j] = Serial.read();
+      Serial.write('1');
+    }
+    pose_real = RealPosition {posein[0], posein[1]};
+    pose_ang = Angle { posein[2] };
+    pose_time = millis();
+    pixels.setPixelColor(6, posein[0], posein[1], posein[2]);
+    pixels.show();
+  } else {
     mstate_reset();
 
     if (cmd == 0x1B) {
@@ -188,19 +342,17 @@ void cmdTreatment(int cmd) {
       closeServo();
       Serial.write('1');
     } else if ((cmd & 0x1F) == 0x18) {
-      int i =7;
+      int i = 7;
       //for (int i = 7; i < 8; i++) {
-        pixels.setPixelColor(i, pixels.Color((cmd & 0x80) ? 255 : 0, (cmd & 0x40) ? 255 : 0, (cmd & 0x20) ? 255 : 0));
-        pixels.show();  // Send the updated pixel colors to the hardware
+      pixels.setPixelColor(i, pixels.Color((cmd & 0x80) ? 255 : 0, (cmd & 0x40) ? 255 : 0, (cmd & 0x20) ? 255 : 0));
+      pixels.show();  // Send the updated pixel colors to the hardware
       //}
       Serial.write('1');
     } else if (cmd == 0b11100000) {
-
-      uint16_t r, g, b, c, colorTemp, lux;
-      colorTemp = colorTemp;
-      lux = lux;
-      tcs.getRawData(&r, &g, &b, &c);
-
+      Serial.write('1');
+      // TODO
+      //Serial.write(grid_colors[GridPosition.current()]);
+      /*
       if ((r > 150) && (g > 150) && (b > 150)) {
         Serial.write('0');
       } else if ((r > 150) && (g > 150)) {
@@ -212,6 +364,7 @@ void cmdTreatment(int cmd) {
       } else if (b > 150) {
         Serial.write('4');
       }
+      */
     } else if (cmd == 0b11100001) {
       // TODO read battery
       Serial.write('4');
@@ -291,7 +444,7 @@ void loop() {
         if ((((m1e_total_count + m1e_count + m2e_total_count + m2e_count) >= m12e_target) && (movement_type == movement_type_e::FORWARD || movement_type == movement_type_e::BACKWARDS))
             // For rotation
             || ((max(m1e_total_count + m1e_count, m2e_total_count + m2e_count) >= m12e_target) && ((movement_type == movement_type_e::LEFT) || (movement_type == movement_type_e::RIGHT)))) {
-          mstate = mstate_e::LOCKED;
+          mstate = mstate_e::BRAKES;
           mstate_timer = millis();
           setBrake();
 
@@ -333,8 +486,8 @@ void loop() {
         break;
 
 
-      case mstate_e::LOCKED:
-        if (millis() - mstate_timer > LOCK_TIME) {
+      case mstate_e::BRAKES:
+        if (millis() - mstate_timer > BRAKE_TIME) {
           mstate = mstate_e::WAITING_POSE;
           mstate_timer = millis();
           setVelocity(0, 0);
@@ -343,15 +496,18 @@ void loop() {
 
       case mstate_e::WAITING_POSE:
         if (pose_time > mstate_timer) {
-          if(1) { //TODO: posicao correta {
-            mstate_reset();
-            pixels.setPixelColor(5, pose[0], pose[1], pose[2]);
+          if (0) {
+            // TODO: surge
+          }
+          if (abs(pose_ang.diff(movement_final_angle)) < ANGLE_MOVE_TOLERANCE) {
+            pixels.setPixelColor(4, 0, 150, 0);
             pixels.show();
-            Serial.write('1');  
-          } else { // Novo movimento
-            mstate_start(movement_type_e::FORWARD, 10);
-            pixels.setPixelColor(5, pose[0], pose[1], pose[2]);
-            pixels.show();                        
+            mstate_reset();
+            Serial.write('1');
+          } else {
+            pixels.setPixelColor(4, 150, 0, 0);
+            pixels.show();
+            mstate_rotation_to(movement_final_angle);
           }
         }
         break;
