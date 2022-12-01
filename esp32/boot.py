f27a7a485b925bc943797b60131d014c0b249602
Martoto <baronwalrus@gmail.com>
2022-12-01 02:09:10 +0000

    boot.py ric

--- esp32/boot.py
+++ esp32/boot.py
@@ -1,20 +1,28 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
+import machine
 import webrepl
 import network
 import time
+from machine import Pin
 webrepl.start()
 
+Pin(4, Pin.OUT).value(1)
+time.sleep(0.1)
+Pin(4, Pin.OUT).value(0)
+time.sleep(0.1)
+
+#try:
 sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
-sta_if.connect("INTELBRAS","LESC12345")
+try:
+  sta_if.connect("INTELBRAS","LESC12345")
+except Exception as e:
+  print("conn", e)
+#except Exception as e:
+#    print("-> ", e)
 #sta_if.connect("lukn23","12345678")
 #sta_if.connect("CLARO5GDCC6B8","azUmycWqc7")
 #sta_if.connect("BIOINFO-IC","b10cp3g1")
 time.sleep(1)
-
-#uart = machine.UART(1, 9600, rx=12, tx=13)
-#uart.init(9600, bits=8, parity=None, stop=1)
-
-
-#test test
+print("end boot")
